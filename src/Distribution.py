5c743f4ef522229d1e54642bbba505460b4e709e
Theo Kempf <theokempf@protonmail.com>
2026-02-18 13:54:53 +0100

    distribution.py more efficient, cleaner

--- src/Distribution.py
+++ src/Distribution.py
@@ -3,45 +3,42 @@ import matplotlib.pyplot as plt
 from global_var import *
 
 
-def get_distribution(data_path : str) -> list:
+def get_distribution(data_path : str) -> dict:
     """
     Takes a directory's path, gets the subdirectories name and counts their files.
     Since the subdirectories can belong to multiple plant categories (ex: apple, grape...) we also retrieve it.
     cat dict-> dirslists -> dirdict
     """
-    distribution_list = []
+    distrib_d = {}
     if data_path[-1] != '/':
         data_path += '/'
     for name in os.listdir(data_path):
         category = name.split('_', 1)[0]
-
-        distribution_list.append({"category" : category,
+        size = len(os.listdir(data_path + name))
+        if category not in distrib_d:
+            distrib_d[category] = []
+        distrib_d[category].append({"category" : category,
                                   "name" : name,
-                                  "size" : len(os.listdir(data_path + name))})
-    return distribution_list
+                                  "size" : size})
+    return distrib_d
 
-def display_distribution(dist_list : list) -> None:
-    cat_list = []
-    for d in dist_list: # cree liste des categories -> pourrait etre fusionné dans get_distribution
-        if d["category"] not in cat_list:
-            cat_list.append(d["category"])
+def display_distribution(distribution : dict) -> None:
     i = 0
-    fig, ax = plt.subplots(len(cat_list), 2, figsize=(14, 8))
+    fig, ax = plt.subplots(len(distribution), 2, figsize=(14, 8))
     fig.canvas.manager.set_window_title("Distribution.py")
-    for cat in cat_list: # cree graph en f des categories
-        # passe autant qu'il y a de cate meme sur ce qui correspond pas -> 50% d'it en trop -> peut faire un {"cat" : id...}
+    for category in distribution:
         size_l = []
         name_l = []
-        for sample in dist_list:
-            if sample["category"] == cat:
-                size_l.append(sample["size"])
-                name_l.append(sample["name"])
+        for category_dict in distribution[category]:
+            size_l.append(category_dict["size"])
+            name_l.append(category_dict["name"])
         wedges, texts, autotexts = ax[i, 0].pie(size_l, labels=name_l, autopct='%1.1f%%')
         colors = [wedge.get_facecolor() for wedge in wedges]
         ax[i, 1].bar(name_l, size_l, color=colors)
-        ax[i, 0].set_title(f"{cat}s :")
+        ax[i, 0].set_title(f"{category} :")
         i += 1
     plt.show()
 
-l = get_distribution(IMAGES_PATH)
-display_distribution(l)
+if __name__ == "__main__":
+    l = get_distribution(IMAGES_PATH)
+    display_distribution(l)
